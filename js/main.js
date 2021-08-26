e4b84ea44cf05d0a114a6057e3df331c3bc54413
AntoFerraro <antodibe_9@hotmail.com>
2021-08-26 20:20:25 -0300

    clase 6

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 class Product{
-    constructor(pasta, precio, stock){
+    constructor(pasta, precio, stock, img){
         this.pasta = pasta || undefined;
         this.precio = precio || undefined;
         this.stock = stock || undefined;
+        this.img = img;
     }
     elStock(porciones){
         if (porciones <= productos[items].stock && porciones > 0){
@@ -11,27 +12,23 @@ class Product{
         } else {
             alert("Disculpe, no tenemos stock para lo que solicitó") ;           
         }
-    }
+    }   
 }
+
+const corazon = new Product(`Ravioles con forma de corazon rellenos con Capresse`, 450, 250, "./imagenes/0.jpeg");
+const cappellacci = new Product(`Cappelacci rellenos de hongos`, 500, 500, "./imagenes/1.jpeg");
+const caramelos = new Product(`Caramelos rellenos de remolacha`, 450, 90,"./imagenes/2.jpeg");
+const pansotti = new Product(`Pansotti rellenos de calabaza`, 500, 120,"./imagenes/3.jpeg");
+const ravioles = new Product(`Ravioles rellenos de queso de cabra y zanahora caramelizada`, 550, 6,"./imagenes/4.jpeg");
+const sobrecitos = new Product(`Sobrecitos rellenos de ricota y verdura`, 500, 50,"./imagenes/5.jpeg");
+const fideos = new Product(`Fideos con forma de flor`, 400, 34,"./imagenes/6.jpeg");
 
 const productos = [
-    new Product(`Ravioles con forma de corazon rellenos con Capresse`, 450, 250),
-    new Product(`Cappelacci rellenos de hongos`, 500, 500),
-    new Product(`Caramelos rellenos de remolacha`, 450, 90),
-    new Product(`Pansotti rellenos de calabaza`, 500, 120),
-    new Product(`Ravioles rellenos de queso de cabra y zanahora caramelizada`, 550, 6),
-    new Product(`Sobrecitos rellenos de ricota y verdura`, 500, 50),
-    new Product(`Fideos con forma de flor`, 400, 34) 
+    corazon, cappellacci, caramelos, pansotti, ravioles, sobrecitos, fideos
 ];
 
-const items = Number(prompt(`Bienvenido, porfavor elija el producto deseado ingresando el nombre en minúscula: \n\n 0 = ${productos[0].pasta} \n 1 = ${productos[1].pasta} \n 2 = ${productos[2].pasta} \n 3 = ${productos[3].pasta} \n 4 = ${productos[4].pasta} \n 5 = ${productos[5].pasta} \n 6 = ${productos[6].pasta}`))
+const items = Number(prompt(`Bienvenido, porfavor elija el producto deseado ingresando el numero al lado de su eleccion: \n\n 0 = ${corazon.pasta} \n 1 = ${cappellacci.pasta} \n 2 = ${caramelos.pasta} \n 3 = ${pansotti.pasta} \n 4 = ${ravioles.pasta} \n 5 = ${sobrecitos.pasta} \n 6 = ${fideos.pasta}`))
 const porciones = Number(prompt("Porfavor elija la cantidad de porciones solicitadas"));
-
-function imagenes() {  //Carga las imagenes
-    let img = document.createElement('img');
-    img.src = "../imagenes/"+items+".jpeg";
-    document.body.appendChild(img); 
-}
 
 function totalCompra(){
     return productos[items].elStock(porciones);
@@ -49,19 +46,46 @@ function resultadoFinal() {
     }
 }
 
+function parsearProd () {
+    const productoJSON = JSON.stringify(productos[items]);
+    console.log(productoJSON)
+}
+parsearProd()
+
 function final () {    
-    document.write(`\n\n <div> 
-    <h1> Gracias por comprar en Tierra de Pastas </h1> </div> \n\n`)
-    imagenes();    
-    document.write(` \n\n <div>
-    <p> Usted ordenó </p> 
-    <b> ${porciones} </b> <p> porciones de: </p> 
-    <b> ${productos[items].pasta} </b> 
-    <p> Su total es de: </p> 
-    <b> $ ${resultadoFinal()} </b> 
+    document.write(`\n\n <div class="laCompra"> 
+    <h1> Gracias por comprar en Tierra de Pastas </h1> \n\n       
+    <img src="${productos[items].img}"/>
+    <p class="parrafo"> Usted ordenó </p> 
+    <b class= "product"> ${porciones} </b> 
+    <p class="parrafo"> porciones de: </p> 
+    <b class= "product"> ${productos[items].pasta} </b> 
+    <p class="parrafo"> Su total es de: </p> 
+    <b class= "product"> $ ${resultadoFinal()} </b> 
     </div>`)    
+    alert("Si quiere seguir comprando agregue al carrito debajo de la descripcion de su compra")
 }
 final();
 
+
+const carrito = []
+let acumuladores = ``;
+productos.forEach((productos) => {
+    acumuladores += `<div class = "laCompra">
+    <img src="${productos.img}"/>        
+    <p class="parrafo"> ${productos.pasta} </p> 
+    <b class= "product"> $ ${productos.precio} </b> 
+    <button class= "addCart" onclick="agregarAlCarrito('${productos.pasta}')"> Agregar al carrito </button>     
+    </div>`        
+})
+
+document.write(acumuladores);
+
+
+function agregarAlCarrito(pasta){    
+    carrito.push(pasta);
+    console.log(carrito);
+
+}
 
 
